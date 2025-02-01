63cfeff44149ae1e25e624275b1aaa38c8058b18
BaileyMeche <baileymeche@uchicago.edu>
2025-01-31 22:44:35 -0600

    docs directory build

--- dodo.py
+++ dodo.py
@@ -19,14 +19,15 @@ OS_TYPE = config("OS_TYPE")
 environ["PYDEVD_DISABLE_FILE_VALIDATION"] = "1"
 def jupyter_execute_notebook(notebook):
     return f"jupyter nbconvert --execute --to notebook --ClearMetadataPreprocessor.enabled=True --log-level WARN --inplace ./src/{notebook}.ipynb"
-def jupyter_to_html(notebook, output_dir=OUTPUT_DIR):
-    return f"jupyter nbconvert --to html --log-level WARN --output-dir={output_dir} ./src/{notebook}.ipynb"
+def jupyter_to_html(notebook, output_dir="docs"):
+    """Convert a Jupyter notebook to an HTML file and save it in the specified directory."""
+    return f'jupyter nbconvert --to html --log-level WARN --output-dir="{output_dir}" ./src/{notebook}.ipynb'
 def jupyter_to_md(notebook, output_dir=OUTPUT_DIR):
     """Requires jupytext"""
     return f"jupytext --to markdown --log-level WARN --output-dir={output_dir} ./src/{notebook}.ipynb"
 def jupyter_to_python(notebook, build_dir):
     """Convert a notebook to a python script"""
-    return f"jupyter nbconvert --log-level WARN --to python ./src/{notebook}.ipynb --output _{notebook}.py --output-dir {build_dir}"
+    return f'jupyter nbconvert --log-level WARN --to python ./src/{notebook}.ipynb --output _{notebook}.py --output-dir "{build_dir}"'
 def jupyter_clear_output(notebook):
     return f"jupyter nbconvert --log-level WARN --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --inplace ./src/{notebook}.ipynb"
 # fmt: on
